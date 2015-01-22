33d9e71d5e611ba3c89819cef32af64ca8642f01
nessup <nessup@gmail.com>
2015-01-21 19:32:22 -0800

    First commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global ModdroidEditor, $*/
+
+
+window.ModdroidEditor = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    ModdroidEditor.init();
+});
